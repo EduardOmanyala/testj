b93c99d7d4c11f27775e4275037c30cb351293be
EduardOmanyala <bestessays001@gmail.com>
2023-05-15 03:06:13 +0300

    second commit

--- articles/settings.py
+++ articles/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cj*@-##_vv&_ycglpbs$t#tq#d#dt2@mh_*e7t0+lim@xl_f1j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.18.98.124']
+ALLOWED_HOSTS = ['3.129.134.27']
 
 
 # Application definition
